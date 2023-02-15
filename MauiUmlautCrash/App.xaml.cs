84f184550aedcbc0d3122930dbed7239ef0d3410
Max Kost <m.kost@ixperienz.com>
2023-02-15 09:58:19 +0100

    Add project files.

---
+++ MauiUmlautCrash/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using UmlautLib;
+
+namespace MauiUmlautCrash;
+
+public partial class App : Application
+{
+	private readonly Tür _tor;
+
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
